62913618e871d40c013566284ebbac1134ae5c37
audrey-audrey <audreyuu@gmail.com>
2020-11-05 22:41:13 +0000

    Testing Cats as a Service - refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,28 @@
 const request = require('request');
 
-
+/**
+ * Parameters for fetchBreedDescription
+ * @param {*} breedName - user specified breed
+ * @param {*} callback - error handling, throw print error or print description of breed
+ */
 const fetchBreedDescription = function(breedName, callback) {
   
   const requestedURL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   
   request(requestedURL, function(error, response, body) {
     // print out error if URL is broken
-    if (error) console.log('Error: ', error);
+    if (error) callback(error, null); // => callback for error
     else {
-      const data = JSON.parse(body);
-      const desc = data[0].description
-  
       // if data is empty, print out message
-      if (desc.length === 0) console.log('Breed not found!!');
-      else console.log(desc);
+      const data = JSON.parse(body);
+
+      if (data.length === 0) {
+        const errMessage = 'Breed not found!';
+        callback(errMessage, null);
+      } else {
+        const breedDescription = data[0].description;
+        callback(null, breedDescription);
+      }
     }
   });
 
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,36 @@
+// breedFetcherTest.js
+
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  // Test case 1 for when breedName is valid
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+  // Test case 2 for when breedName is non-existant and error is expected
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('random', (err, desc) => {
+      // we expect error to say Breed not found!
+      assert.equal(err, 'Breed not found!');
+
+      const expectedDesc = null;
+
+      // compare returned description, should be null
+      assert.equal(expectedDesc, desc);
+
+      done();
+    });
+  });
+});
